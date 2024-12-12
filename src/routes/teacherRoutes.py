4be90324ae972dc18dce3d5f7dd1e94d105d29f9
Niormon <74582145+Niormon@users.noreply.github.com>
2024-12-12 10:32:19 -0500

    Feat: Autenticacion de permisos de profesores

--- src/routes/teacherRoutes.py
+++ src/routes/teacherRoutes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.database import get_db
+from src.utils.authPerm import permiso_requerido
 from src.schemas.teacherSchema import TeacherCreate, TeacherUpdate, TeacherResponse
 from uuid import UUID
 from src.controllers.teacherController import (
@@ -10,21 +11,31 @@ from src.controllers.teacherController import (
 TEACHER_ROUTES = APIRouter()
 
 @TEACHER_ROUTES.get("/teachers/", response_model=list[TeacherResponse])
-async def list_teachers(db: AsyncSession = Depends(get_db)):
+async def list_teachers(db: AsyncSession = Depends(get_db),
+    _: None = Depends(permiso_requerido("Consultar"))
+):
     return await get_teachers(db)
 
 @TEACHER_ROUTES.get("/teachers/{teacher_id}", response_model=TeacherResponse)
-async def get_teacher_by_id(teacher_id: UUID, db: AsyncSession = Depends(get_db)):
+async def get_teacher_by_id(teacher_id: UUID, db: AsyncSession = Depends(get_db),
+    _: None = Depends(permiso_requerido("Consultar"))
+):
     return await get_teacher(db, teacher_id)
 
 @TEACHER_ROUTES.post("/teachers/", response_model=TeacherResponse)
-async def create_teacher_endpoint(teacher: TeacherCreate, db: AsyncSession = Depends(get_db)):
+async def create_teacher_endpoint(teacher: TeacherCreate, db: AsyncSession = Depends(get_db),
+    _: None = Depends(permiso_requerido("Operar"))
+):
     return await create_teacher(db, teacher)
 
 @TEACHER_ROUTES.put("/teachers/{teacher_id}", response_model=TeacherResponse)
-async def update_teacher_endpoint(teacher_id: UUID, teacher: TeacherUpdate, db: AsyncSession = Depends(get_db)):
+async def update_teacher_endpoint(teacher_id: UUID, teacher: TeacherUpdate, db: AsyncSession = Depends(get_db),
+    _: None = Depends(permiso_requerido("Operar"))
+):
     return await update_teacher(db, teacher_id, teacher)
 
 @TEACHER_ROUTES.delete("/teachers/{teacher_id}")
-async def delete_teacher_endpoint(teacher_id: UUID, db: AsyncSession = Depends(get_db)):
+async def delete_teacher_endpoint(teacher_id: UUID, db: AsyncSession = Depends(get_db),
+    _: None = Depends(permiso_requerido("Operar"))
+):
     return await delete_teacher(db, teacher_id)
